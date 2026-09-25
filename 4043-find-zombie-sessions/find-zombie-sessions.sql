80567e7665f2b98685f6b363462462de074ff300
Sohel Bhongade <bhongadesohel121219@gmail.com>
2026-09-25 23:19:48 +0530

    Time: 362 ms (43.76%) | Memory: 0B (100.00%) - LeetSync

--- 4043-find-zombie-sessions/find-zombie-sessions.sql
+++ 4043-find-zombie-sessions/find-zombie-sessions.sql
@@ -1,22 +1,16 @@
-WITH session_summary AS (
-    SELECT 
-        session_id,
-        user_id,
-        SUM(CASE WHEN event_type = 'scroll' THEN 1 ELSE 0 END) AS scroll_count,
-        SUM(CASE WHEN event_type = 'click' THEN 1 ELSE 0 END) AS click_count,
-        SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END) AS purchase_count,
-        TIMESTAMPDIFF(MINUTE, MIN(event_timestamp), MAX(event_timestamp)) AS session_duration_minutes
-    FROM app_events
-    GROUP BY session_id, user_id
-)
-SELECT 
-    session_id,
-    user_id,
-    session_duration_minutes,
-    scroll_count
-FROM session_summary
-WHERE purchase_count = 0
-  AND scroll_count >= 5
-  AND (click_count / scroll_count) < 0.2
-  AND session_duration_minutes >= 30
-ORDER BY scroll_count DESC, session_id ASC;
+# Write your MySQL query statement below
+with x as (select session_id ,user_id , 
+TIMESTAMPDIFF(MINUTE, min(event_timestamp), max(event_timestamp)) as session_duration_minutes ,
+sum(case when event_type='scroll' then 1 else 0 end) 
+ as scroll_count ,
+sum(case when event_type='click' then 1 else 0 end) 
+ as click_count ,
+sum(case when event_type='purchase' then 1 else 0 end) 
+ as purchase_count
+from app_events
+group by  session_id ,user_id )
+
+select session_id ,user_id ,session_duration_minutes , scroll_count
+from x
+where purchase_count = 0 and scroll_count >= 5 and click_count/scroll_count < 0.2 and session_duration_minutes  >= 30
+order by scroll_count desc, session_id asc
